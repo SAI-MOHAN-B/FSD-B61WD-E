8c6096dd91e36b72927aeb11e1887f6f7c64f395
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-01 11:09:43 +0530

    Update Spread and Rest Parameter.js

--- Spread and Rest Parameter.js
+++ Spread and Rest Parameter.js
@@ -14,3 +14,10 @@ console.log(foo(...arr));
 var str = "guvi geek"
 var res = [...str]
 console.log(res);
+
+
+// Combining the arrays
+var arr1  = ["abc","def"]
+var arr2 = ["ghi","jkl"]
+var arr3 = [...arr1,...arr2]
+console.log(arr3);
