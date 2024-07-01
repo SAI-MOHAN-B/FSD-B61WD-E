d4ab62761d5f6bbd447f673a1b9a3f248bc6854a
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-01 11:04:30 +0530

    Create Spread and Rest Parameter.js

---
+++ Spread and Rest Parameter.js
@@ -0,0 +1,16 @@
+// Spread Operator
+// ...
+
+// expanding the arrays
+function foo(a,b){
+return a+b;
+}
+let arr = ["hello","guvi"]
+console.log(arr);
+console.log(...arr);
+console.log(foo(...arr));
+
+// Strings:
+var str = "guvi geek"
+var res = [...str]
+console.log(res);
