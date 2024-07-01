4e61f18445cf3ccfb95b8e81fef035c1e394c6e6
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-01 11:14:34 +0530

    Update Spread and Rest Parameter.js

--- Spread and Rest Parameter.js
+++ Spread and Rest Parameter.js
@@ -21,3 +21,9 @@ var arr1  = ["abc","def"]
 var arr2 = ["ghi","jkl"]
 var arr3 = [...arr1,...arr2]
 console.log(arr3);
+
+
+// combining the objects
+let obj1 = {name:"guvi",age:23}
+let obj2 = {...obj1,sal:34000,year:2018}
+console.log(obj2);
