1ff97774cca2843f9c70fa4a1091885b658cee92
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-01 11:22:26 +0530

    Update Spread and Rest Parameter.js

--- Spread and Rest Parameter.js
+++ Spread and Rest Parameter.js
@@ -27,3 +27,12 @@ console.log(arr3);
 let obj1 = {name:"guvi",age:23}
 let obj2 = {...obj1,sal:34000,year:2018}
 console.log(obj2);
+
+// rest paramaters:
+// rest meaning remaining parameters
+// rest parameter are always applicable to functions
+// The retuirn type of the rest parameter always an array
+function foo(a,b,c,...remain){
+return a+b+c;
+}
+console.log(foo(12,13,14,15,16,17,18));
