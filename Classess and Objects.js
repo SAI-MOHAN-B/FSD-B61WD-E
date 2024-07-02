8351b149fa631a3bc7e3cec1504846e325e0f581
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-02 11:15:16 +0530

    Create Classess and Objects.js

---
+++ Classess and Objects.js
@@ -0,0 +1,26 @@
+// Engine,4 tyres, color,steering
+// class is a keyword needed to create the class
+class Car{
+// Constructor is a special way of assigning values to 
+// Objects
+constructor(color,brand){
+this.color = color;
+this.brand = brand;
+}
+print_color(){
+return "the color of the car is"+this.color;
+}
+
+}
+
+// Object:
+var c1 = new Car("white","Audi");
+// new is a inbuilt word
+// it will create a object
+var c2 =  new Car("black","RollceRoyce");
+// if we  write a function within the class 
+// then we call it as method
+// there is no keyword called function
+
+// Syntax: referencevariable.methodname()
+console.log(c1.print_color());
