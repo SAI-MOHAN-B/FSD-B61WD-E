3519383c810ab512fa6729852561db7773ef3778
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-22 11:41:26 +0530

    Adding Module 04

---
+++ Async Programing/script.js
@@ -0,0 +1,10 @@
+function f1() {
+    function f2() {
+        function f3() {
+            function f4() {
+                console.log("Hello World!!!");
+            }
+        }
+    }
+}
+f1();
